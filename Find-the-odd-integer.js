742638cf1d3bcb5510e7fcae704a7865b453e79e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-19 16:43:30 -0700

    Update Find-the-odd-integer.js

--- Find-the-odd-integer.js
+++ Find-the-odd-integer.js
@@ -14,3 +14,21 @@ Examples:
 
 
 // Solution
+
+function findOdd(arr) {
+  let result, num = 0;
+
+  arr = arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i+1]) {
+      num++;
+    } else {
+      num++;
+      if (num % 2 != 0) {
+        result = arr[i];
+        break;
+      }
+    }
+  }
+  return result;
+}
